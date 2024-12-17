bfd4b8eb4bed431e1932ae6a72bbdabf784239b5
BugHunter-X <askzenai@gmail.com>
2024-12-17 19:30:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// This is a Tailwind CSS class that causes an error if not used correctly
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg shadow-lg">
+  <p>This is some text.</p>
+</div>
+```
